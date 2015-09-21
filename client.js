4e3920fd0f27658758c27d5222120e4f381f08a7
AlexKVal <alexkval@gmail.com>
2015-09-21 20:55:42 +0300

    [added] products and product

--- client.js
+++ client.js
@@ -2,6 +2,48 @@ var App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
 
+App.PRODUCTS = [
+  {
+    title: 'Kingling',
+    price: 249,
+    description: 'Easily...',
+    isOnSale: false,
+    image: 'kindling.png'
+  },
+  {
+    title: 'Flint',
+    price: 99,
+    description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
+    isOnSale: true,
+    image: 'flint.png'
+  }
+];
+
 App.Router.map(function() {
   this.route('about', {path: 'aboutus'}); // path '#/aboutus', route 'about'
+  this.route('products', {path: 'items'}, function() {
+    this.route('product', {path: ':title'});
+  });
+});
+
+App.ProductsRoute = Ember.Route.extend({
+  model: function() {
+    return App.PRODUCTS;
+  }
+});
+
+App.ProductRoute = Ember.Route.extend({
+  model: function(params) {
+    var result = App.PRODUCTS.findBy('title', params.title);
+    console.log(result);
+    return result;
+  }
+});
+
+App.IndexController = Ember.Controller.extend({
+  counter: function() {
+    return +new Date();
+  }.property(),
+  src: '/image.jpg',
+  alt: 'This is the image'
 });
