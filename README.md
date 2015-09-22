4afbf84974a783099eb05f910f017e117447b8ae
AlexKVal <alexkval@gmail.com>
2015-09-22 10:05:33 +0300

    [added] reviews

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Ember CS learning project
 
 ```
-> bower install ember ember-data jquery handlebars bootstrap express
-> node server.js
+> bower install ember ember-data jquery handlebars bootstrap express lodash nodemon
+> nodemon server.js
 > open http://localhost:8000
 ```
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var _ = require('lodash');
 
 var app = express();
 
@@ -9,26 +10,50 @@ var products = [
     price: 249,
     description: 'Easily...',
     isOnSale: false,
-    image: 'kindling.png'
+    image: 'img/kindling.jpeg',
+    reviews: [100,101]
   },
   { id: 2,
     title: 'Flint',
     price: 99,
     description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
     isOnSale: true,
-    image: 'flint.png'
+    image: 'img/flint.jpeg',
+    reviews: []
   },
   { id: 3,
     title: 'Third',
     price: 333,
     description: 'This is the third`s description.',
     isOnSale: true,
-    image: 'third.png'
+    image: 'img/third.jpeg',
+    reviews: []
+  }
+];
+
+var reviews = [
+  {
+    id: 100,
+    product: 1,
+    text: 'Started a fire in no time!'
+  },
+  {
+    id: 101,
+    product: 1,
+    text: 'Not the brightest flame, but warm!'
   }
 ];
 
 app.get('/products', function(req, res) {
   res.json(products);
+})
+
+app.get('/reviews', function(req, res) {
+  res.json(reviews);
+})
+
+app.get('/reviews/:id', function(req, res) {
+  res.json(_.find(reviews, 'id', +req.params.id));
 })
 
 app.use('/', express.static(__dirname));
